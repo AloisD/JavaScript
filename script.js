2381d2e7b46014961df6c42aa6cf18a6f105bb7b
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2021-11-30 09:18:48 +0100

    new input array and new calcAvg function

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 "use strict";
 
 const bills = [
-  125,
-  555,
-  44
+  22,
+  295,
+  176,
+  440,
+  37,
+  105,
+  10,
+  1100,
+  86,
+  52
 ];
 
 let tips = [];
@@ -16,4 +23,6 @@ for (let i = 0; i < bills.length; i ++) {
   totals[i] = tips[i] + bills[i];
 }
 
-console.log(totals);
+const calcAverage = (inputArray) => inputArray.reduce((acc, curr) => acc + curr) / inputArray.length;
+
+console.log(calcAverage(totals));
