5695ffbfe3a72ef9c796692670c7778bcc2d5cac
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2021-11-30 10:51:31 +0100

    decode using loops

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 "use strict";
 
-const bills = [
-  22,
-  295,
-  176,
-  440,
-  37,
-  105,
-  10,
-  1100,
-  86,
-  52
-];
+// Welcome to DonkeyDecode, your mission is to decode the full message. Good luck
 
-let tips = [];
-let totals = [];
+// EX 01 -  Use a loop to remove the 'X' and use console.log to reveal the message 
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
 
-const calcTip = (bill) => bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+// Your code goes here
 
-for (let i = 0; i < bills.length; i ++) {
-  tips[i] = calcTip(bills[i]);
-  totals[i] = tips[i] + bills[i];
+// this feels better but the instruction was to use a loop
+/* function removeX(xOrNot) {
+  return xOrNot != "X";
 }
 
-const calcAverage = (inputArray) => inputArray.reduce((acc, curr) => acc + curr) / inputArray.length;
+console.log(hiddenMessage.filter(removeX)); */
 
-console.log(calcAverage(totals));
+function removeXAndEven(inputArray) {
+  for (let i = 0; i < inputArray.length; i ++) {
+    if (inputArray[i] != "X" && inputArray[i] % 2 != 0) { console.log(inputArray[i]); }
+  }
+}
+
+removeXAndEven(hiddenMessage);
+
+// EX 02 - Remove the 'X' and the even numbers
+// Hint: As this is a string you should find a way to convert into an array.
+
+const hiddenCode = "XXXXXXXXXXXXXXXXXXXXXXXXXXXCXXXXXXXOXXXXXDXXXXXXXXXXXXXXXEXXXX XXXXXXXXXXXXXXX:X0XXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX3XXXXXX2XXXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX6XXXXXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX7XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXXXXXXXXX8XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX8XXXXXXXXXXX4XXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXX9XXXX9XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXX4XXXXXXXXXXX6XXXXXXXXX2XXXXXXXX3XXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXX"
+
+// Your code goes here
+
+const hiddenCodeArray = [...hiddenCode];
+
+removeXAndEven(hiddenCodeArray);
