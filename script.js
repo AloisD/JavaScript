623da5280952385f6a54200dd889fdee472926d8
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2021-11-30 16:49:11 +0100

    challenge11

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 "use strict";
 
-/* Crée un objet billyTheCat
-il doit avoir une propriété name,
-une propriété color,
-une propriété favouriteFoods (un tableau avec plusieurs entrées),
-une propriété isHungry définie à true
-et une méthode meow qui imprime “Meooow” dans la console
-Ensuite, crée une variable selectedProperty et, à l’aide de prompt, demande à l’utilisateur de choisir la propriété de l’objet qu’il veut afficher.
-Enfin, utilise console.log pour afficher la bonne propriété (en utilisant des crochets []). */
+/* Tâches :
+Pour chacun, créer un objet avec des propriétés pour leurs prénom, poids et taille. ✓
+Créer une méthode ‘calcBMI’ dans chacun des objets permettant de calculer l’IMC. Stockez la valeur calculée dans une propriété de l’objet et aussi la retourner dans la méthode.
+Afficher dans la console qui a l’IMC le plus élevé de cette manière : “L’IMC de Bernard (20,2) est plus élevé que celui de Marcel (19,3)”
+Données de test :
+Données 1 : Bernard pèse 78kg et mesure 1,69m. Marcel pèse 92kg et mesure 1,95m.
+Données 2 : Bernard pèse 95kg et mesure 1,88m. Marcel pèse 85kg et mesure 1,76m. */
 
-const billyTheCat = {
-  name: "billy",
-  color: "",
-  favouriteFoods: [],
-  isHungry: true,
-  meow: function() {
-    console.log("Meooow");
+const bernard = {
+  weight: 78,
+  height: 1.69,
+  name: "Bernard",
+  bmi: undefined,
+
+  calcBMI: function () {
+    this.bmi = Math.round((this.weight / this.height ** 2) * 10) / 10;
+    return this.bmi;
   }
 }
 
-const selectedProperty = prompt('Tape la propriété que tu veux afficher') ;
-console.log(billyTheCat[selectedProperty]) ;
+const marcel = {
+  weight: 92,
+  height: 1.95,
+  name: "Marcel",
+  bmi: undefined,
+
+  calcBMI: function () {
+    this.bmi = Math.round((this.weight / this.height ** 2) * 10) / 10;
+    return this.bmi;
+  }
+}
+
+function compareBMI(person1, person2) {
+  person1.calcBMI();
+  person2.calcBMI();
+
+  // this could be made dryer by figuring out who's got the higher IMC and then logging the text
+  if (person1.bmi > person2.bmi) {
+    console.log("L’IMC de " + person1.name + " (" + person1.bmi + ") est plus élevé que celui de " + person2.name + " (" + person2.bmi + ")");
+  } else if (person1.bmi < person2.bmi) {
+    console.log("L’IMC de " + person2.name + " (" + person2.bmi + ") est plus élevé que celui de " + person1.name + " (" + person1.bmi + ")");
+  } else {
+    console.log(person1.name + " et " + person2.name + "ont le même IMC (" + person1.bmi + ")");
+  }
+}
+
+compareBMI(bernard, marcel);
+
+bernard.weight = 95;
+bernard.height = 1.88;
+marcel.weight = 85;
+marcel.height = 1.76;
+
+compareBMI(bernard, marcel);
